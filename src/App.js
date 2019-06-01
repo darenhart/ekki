3a373d5bc5a215d06d23403497c53984451ffc88
Daniel Werle Arenhart <arenhart.daniel@gmail.com>
2019-06-01 12:28:17 -0300

    Initial files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+//import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
